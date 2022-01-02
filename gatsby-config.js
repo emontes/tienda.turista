32a4fbd2455f7652340e4fcb3f0d4724c9724218
emontes <emontes@turista.com.mx>
2022-01-02 05:43:32 -0500

    primer commit

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+require("dotenv").config()
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: "Tienda Turista",
+    siteTitleDefault: "Tienda del Turista",
+    siteUrl: "https://shopify-demo.gatsbyjs.com",
+    hrefLang: "en",
+    siteDescription:
+      "A Gatsby starter using the latest Shopify plugin showcasing a store with product overview, individual product pages, and a cart.",
+    siteImage: "/default-og-image.jpg",
+    twitter: "@gatsbyjs",
+  },
+  flags: {
+    FAST_DEV: true,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-shopify",
+      options: {
+        password: process.env.SHOPIFY_SHOP_PASSWORD,
+        storeUrl: process.env.GATSBY_SHOPIFY_STORE_URL,
+        shopifyConnections: ["collections"],
+      },
+    },
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ].filter(Boolean),
+}
