9b29d238f1e0ee44cbd9735c00e690e2abc87f9f
emontes <emontes@turista.com.mx>
2022-01-07 10:24:18 -0500

    ajustes a tipografIas de las policies

--- src/templates/policy-template.js
+++ src/templates/policy-template.js
@@ -1,97 +1,63 @@
-import React from "react";
-import {Layout} from "../components/Layout";
-import styled from "styled-components";
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import React from "react"
+import { Layout } from "../components/Layout"
+import styled from "styled-components"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 // import Banner from "../components/Banner";
-import { graphql } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
+import { graphql } from "gatsby"
+import { MDXRenderer } from "gatsby-plugin-mdx"
 const PolicyTemplate = ({ data }) => {
   const {
     mdx: {
       frontmatter: { title, date },
       body,
     },
-  } = data;
+  } = data
 
   return (
     <Layout>
       <Wrapper>
         {/* post info */}
         <article>
-          
-          <div className="post-info">
-            
-            <h2>{title}</h2>
-            <div className="underline"></div>
-          </div>
+          <h1>{title}</h1>
+
           <MDXRenderer>{body}</MDXRenderer>
-        </article>
-        {/* banner */}
-        <article>
-          {/* <Banner /> */}
         </article>
       </Wrapper>
     </Layout>
-  );
-};
+  )
+}
 
 const Wrapper = styled.section`
   width: 85vw;
-  max-width: 1100px;
   margin: 0 auto;
   margin-bottom: 4rem;
+  font-size: 1.1rem;
+  white-space: pre-line;
 
-  .post-info {
-    margin: 2rem 0 4rem 0;
-    text-align: center;
-    span {
-      background: var(--clr-primary-5);
-      color: var(--clr-white);
-      border-radius: var(--radius);
-      padding: 0.25rem 0.5rem;
-      text-transform: uppercase;
-      letter-spacing: var(--spacing);
-    }
-    h2 {
-      margin: 1.25rem 0;
-      font-weight: 400;
-    }
-    p {
-      color: var(--clr-grey-5);
-    }
-    .underline {
-      width: 5rem;
-      height: 1px;
-      background: var(--clr-grey-9);
-      margin: 0 auto;
-      margin-bottom: 1rem;
-    }
+  h1 {
+    font-size: 2.8rem;
   }
+  h2 {
+    font-size: 1.6rem;
+    margin-top: 1.6rem;
+  }
+  p {
+    margin-top: 1.1rem;
+  }
+
   @media (min-width: 992px) {
     & {
       width: 92vw;
     }
-    .main-img {
-      width: 75%;
-      display: block;
-      margin: 0 auto;
-    }
   }
-  @media (min-width: 1170px) {
-    & {
-      display: grid;
-      grid-template-columns: 1fr 200px;
-      column-gap: 4rem;
-    }
-  }
-`;
+`
 
-export default PolicyTemplate;
+export default PolicyTemplate
 
 export const query = graphql`
   query GetSinglePost($slug: String) {
     mdx(frontmatter: { slug: { eq: $slug } }) {
-      frontmatter {      
+      frontmatter {
         date(formatString: "MMMM Do, YYYY")
         slug
         title
@@ -99,4 +65,4 @@ export const query = graphql`
       body
     }
   }
-`;
+`
